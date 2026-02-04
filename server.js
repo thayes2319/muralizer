724895f66bcb63986612e953fbe34a2084887222
thayes2319 <thomas@gohw.net>
2026-02-03 21:19:32 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// redeploy8
+// redeploy9
 
 import express from "express";
 import fetch from "node-fetch";
@@ -68,7 +68,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
 
